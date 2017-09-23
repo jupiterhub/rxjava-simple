f8b6baf689880e805678fbb9b039edc4fab3ac61
jupiterhub <jupiter.adverts@gmail.com>
2017-09-23 23:04:00 +0800

    sample for http request and handling it

--- README.md
+++ README.md
@@ -13,9 +13,10 @@ This project however uses JDK8 as JDK9 is not compatible with gradle at this poi
 * Flowable (has backpressure, meaning it can create another request)
 * Observable (no backpressure, only dispose)
 * Publisher, Subscriber, Subscription, Processor (has the publisher and subscriber)
-* More details on Backpressure http://reactivex.io/documentation/operators/backpressure.html
-
-#### Specialized classes (No backpressure)
 * Completable - complete or error )
 * Single - item or error
-* Maybe - item, complete, or error
+* Maybe - item, complete, or erro
+* More details on Backpressure http://reactivex.io/documentation/operators/backpressure.html
+
+#### Notable Operators (methods)
+* .map(s -> s.toUpperCase) applies operation to a data
--- src/test/java/ReactiveApiTest.java
+++ src/test/java/ReactiveApiTest.java
@@ -1,4 +1,8 @@
+import com.sun.deploy.net.HttpResponse;
 import io.reactivex.*;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -84,6 +88,112 @@ public class ReactiveApiTest {
 
     @Test
     public void testCreateSourcesFromCreate() {
+        Observable.create(new ObservableOnSubscribe<String>() {
 
+            // emmitter is the person listening
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                e.onNext("Hello");
+                e.onComplete();
+            }
+        });
+
+        // Lambda version
+        Observable.create(e -> {
+            // For callable you cannot call onNext multiple times
+            e.onNext("Hello");
+            e.onNext("World");
+            e.onComplete();
+        });
+    }
+
+    @Test
+    public void testCreateSourcesWithNetworkCall() {
+        // something like this
+        /*
+        Observable.create(e -> {
+            // Call call = client.newCall(request);
+            call.execute(new Callback() {
+                void onResponse(HttpResponse resp) {
+                    e.onNext(resp.getResponseBody()); // <------ Call the event after the response
+                    e.onComplete();
+                }
+
+                void onFailure(HttpResponse resp) {
+                    e.onError(); // <------- Call the event after response
+                }
+            })
+        });
+        */
+    }
+
+    @Test
+    public void testDisposableObserver() {
+        // by default there is a 4th method called onSubscribe(Disposable d)
+        // This class handles that for you
+
+        // applies to all types Single, Maybe, Flowable, Completable
+        Observable<String> hello = Observable.just("Hello");
+
+        DisposableObserver observer = new DisposableObserver<String>() {
+            @Override public void onNext(String s) { }
+            @Override public void onError(Throwable e) { }
+            @Override public void onComplete() { }
+        };
+
+        // subscribeWith will return the disposable object
+        DisposableObserver disposableObserver = hello.subscribeWith(observer);
+        disposableObserver.dispose();
+    }
+
+    @Test
+    public void userReactive() {
+        UserManagerReactive userManagerReactive = new UserManagerReactive() {
+            @Override
+            public Observable<User> getUser() {
+                return null;
+            }
+
+            @Override
+            public Completable setName(String name) {
+                return null;
+            }
+
+            @Override
+            public Completable setAge(int age) {
+                return null;
+            }
+        };
+
+        Observable<User> user = userManagerReactive.getUser();
+
+        // use the background thread good for IO
+        // There are more @ http://reactivex.io/RxJava/2.x/javadoc/
+        user.observeOn(Schedulers.io());
+    }
+
+    @Test
+    public void testHttpResponse() {
+        // network call is async
+        // create an observeOn and wait for the response
+        // that way the main thread is not blocked
+        // and the data can be rendered once it is ready
+
+        Observable<String> result = Observable.fromCallable(() -> {
+            // Pretend we return HTTP RESPONSE ----> client.callApi();
+            return new HttpResponse();    // sample we have an api call
+        })
+        .subscribeOn(Schedulers.io())  // subscribe on backgoround thread
+        .map(response -> response.getBody())   // imagine we are calling response body
+        .observeOn(Schedulers.single()); // observer the result on the main thread
+        // .map(...);       order matters. if placed here, it will execute on main thread
+    }
+
+
+
+    class HttpResponse {
+        public String getBody() {
+            return "response content";
+        }
     }
 }
